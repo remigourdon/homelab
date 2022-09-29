af4b5b34cfad371491ae22b7756ae7d10ef6a8ea
remigourdon <gourdon.remi@gmail.com>
2022-09-28 23:47:15 -0400

    Create resources for 5 K3s nodes

--- iac/terraform/main.tf
+++ iac/terraform/main.tf
@@ -13,9 +13,30 @@ provider "proxmox" {
   pm_tls_insecure = true
 }
 
-resource "proxmox_vm_qemu" "resource-name" {
-  name        = "ubuntu-test"
-  target_node = "pve-1"
-  clone       = "jammy-server-cloudimg-amd64"
-  full_clone  = true
+locals {
+  nodes = {
+    "k3s-01" = {
+      mac_address = "74:3e:14:61:69:32"
+    },
+    "k3s-02" = {
+      mac_address = "83:6b:28:c2:f2:7c"
+    },
+    "k3s-03" = {
+      mac_address = "97:cd:33:40:7b:de"
+    },
+    "k3s-04" = {
+      mac_address = "a2:9f:15:c8:87:f6"
+    },
+    "k3s-05" = {
+      mac_address = "b6:dc:7e:00:f5:f9"
+    }
+  }
+}
+
+module "k3s-node" {
+  for_each     = local.nodes
+  source       = "./modules/k3s-node"
+  name         = each.key
+  proxmox_node = "pve-1"
+  mac_address  = each.value.mac_address
 }
