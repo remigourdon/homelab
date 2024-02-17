2d265a13ae2bd21cd21e13134ab73fbb6cbf7065
remigourdon <gourdon.remi@gmail.com>
2024-02-17 16:14:57 -0500

    Set up Terraform Cloud workspace

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,10 @@
 terraform {
+  cloud {
+    organization = "remigourdon"
+    workspaces {
+      name = "homelab"
+    }
+  }
   required_providers {
     proxmox = {
       source  = "Telmate/proxmox"
